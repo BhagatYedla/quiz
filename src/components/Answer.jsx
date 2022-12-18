6d6e456b0b09244d83a7d6520d75bd025fd9fae0
Bhagat <bhagat@Apples-MacBook-Pro.local>
2022-12-18 13:15:43 +0530

    fix: few imporvemnets in implementation

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -1,16 +1,19 @@
 import { Button, CardActions, CardContent, TextField } from "@mui/material";
 import { useState } from "react";
 
-export default function Answer({ question, answer, handleSubmit }) {
+export default function Answer({ question, handleSubmit }) {
   const [ans, setAns] = useState("");
 
   return (
     <>
     <CardContent>
-      <TextField size="small" sx={{width: '50%'}}  placeholder="Answer" onChange={(e) => setAns(e.target.value)}/>
+      <TextField size="small" sx={{width: '50%'}} value={ans}  placeholder="Answer" onChange={(e) => setAns(e.target.value)}/>
     </CardContent>
     <CardActions>
-      <Button size="small" variant="contained" onClick={() => handleSubmit(question, answer, ans)}>
+      <Button size="small" variant="contained" onClick={() => {
+        handleSubmit(question, ans)
+        setAns('');
+        }}>
         Submit
       </Button>
     </CardActions>
--- src/containers/Quiz.jsx
+++ src/containers/Quiz.jsx
@@ -5,36 +5,10 @@ import Question from "../components/Question";
 const sha1 = require("js-sha1");
 const qaMap = new Map();
 
-const currentQuestion = [
-  {
-    answerSha1: "5c8452354c261b52e6dcf7f94b80ea5a7bceb677",
-    question:
-      "What do people mean when type the letters 'SMH' in a message on the internet?"
-  },
-  {
-    answerSha1: "088e4a2e6f0c20048cd3e53c639c7092bffb8524",
-    question:
-      "Which country's flag can be described as 'Green with a vertical white band on the left side. The green section contains a white crescent and star.'?"
-  },
-  {
-    answerSha1: "b79445b10bd5bc34cbebf63355101dbdb420aa0e",
-    question: "Which director directed Gangs of New York?"
-  },
-  {
-    answerSha1: "fd26fb6ff5aa10eaddad0b2c832139dbe052c9d7",
-    question:
-      "Which philosopher famously said 'This is patently absurd; but whoever wishes to become a philosopher must learn not to be frightened by absurdities'?"
-  },
-  {
-    answerSha1: "e8002c169040af08d8b4ed2f0d636b840f335f9b",
-    question: "What is Xylology the study of?"
-  }
-];
-
 export default function Quiz() {
   const [score, setScore] = useState(0);
   const [attempts, setAttempts] = useState(3);
-  const [currentQuestions, setCurrentQuestions] = useState(currentQuestion);
+  const [currentQuestions, setCurrentQuestions] = useState([]);
   const [open, setOpen] = useState(false);
 
   useEffect(() => {
@@ -42,29 +16,31 @@ export default function Quiz() {
   }, []);
 
   const fetchQuestionAnswer = () => {
-    fetch("https://dummyjson.com/products/1")
+    fetch("https://eok9ha49itquif.m.pipedream.net")
       .then((res) => res.json())
       .then((data) => {
         if (data?.questions?.length) {
+          const questionSet = [];
           data.questions.forEach((qa) => {
             if (!qaMap.has(qa.question)) {
-              qaMap.put(qa.question, qa.answer);
-              currentQuestions.push(qa.question);
+              qaMap.set(qa.question, qa.answerSha1);
+              questionSet.push(qa.question);
             }
           });
+          setCurrentQuestions(questionSet);
         } else {
             setOpen(true);
         }
       });
   };
 
-  const handleSubmit = (question, answerSha, answer) => {
+  const handleSubmit = (question, answer) => {
     if (answer) {
       const current = answer.toLowerCase().trim();
       const shaAnswer = sha1(current);
-      if (shaAnswer === answerSha) {
+      if (shaAnswer === qaMap.get(question)) {
         setScore((prevState) => prevState + 1);
-        const currentState = [...currentQuestion];
+        const currentState = [...currentQuestions];
         currentState.pop();
         if(currentState.length <= 0){
             fetchQuestionAnswer();
@@ -94,11 +70,10 @@ export default function Quiz() {
     {currentQuestions.length > 0 && (
     <Card>
         <Question
-        question={currentQuestions[currentQuestions.length - 1]?.question}
+        question={currentQuestions[currentQuestions.length - 1]}
         />
         <Answer
-        question={currentQuestions[currentQuestions.length - 1]?.question}
-        answer={currentQuestions[currentQuestions.length - 1]?.answerSha1}
+        question={currentQuestions[currentQuestions.length - 1]}
         handleSubmit={handleSubmit}
         />
     </Card>
